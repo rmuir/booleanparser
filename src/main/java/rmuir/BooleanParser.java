f235d995089c1a7d3878af2dac52d5bc731a02aa
Robert Muir <rmuir@apache.org>
2015-06-02 16:47:39 -0400

    Initial (and hopefully final) revision.

---
+++ src/main/java/rmuir/BooleanParser.java
@@ -0,0 +1,50 @@
+package rmuir;
+
+import java.text.ParseException;
+
+/*
+ * Copyright (c) 2015 Robert Muir. 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/**
+ * Boolean parser that does not suck.
+ */
+public final class BooleanParser {
+  
+  /**
+   * Parses a boolean value, the way you would expect.
+   * @param string text to parse. must be {@code true} or {@code false}.
+   * @return parsed boolean value
+   * @throws ParseException if {@code string} is something other than {@code true} or {@code false}.
+   * @throws NullPointerException if {@code string} is {@code null}.
+   */
+  public static boolean parseBoolean(String string) throws ParseException {
+    switch (string) {
+      case "true": return true;
+      case "false": return false;
+      default: throw new ParseException("Invalid boolean value: '" + string + "'", 0);
+    }
+  }
+}
